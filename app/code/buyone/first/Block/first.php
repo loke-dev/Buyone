56ed7f140f3a6492a0ee91213afb0efe678fdbdf
Loke Carlsson <lc222ak@student.lnu.se>
2016-04-29 15:22:05 +0200

    first test

---
+++ app/code/buyone/first/Block/first.php
@@ -0,0 +1,10 @@
+<?php
+namespace buyone\first\Block;
+
+class HelloWorld extends \Magento\Framework\View\Element\Template
+{
+	public function _prepareLayout()
+	{
+		return parent::_prepareLayout();
+	}
+}
