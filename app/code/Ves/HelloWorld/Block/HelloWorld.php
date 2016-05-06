25dff8afd1443fbc804957b849bf4438b55646ed
Loke Carlsson <lc222ak@student.lnu.se>
2016-05-06 16:19:15 +0200

    Added local repo

---
+++ app/code/Ves/HelloWorld/Block/HelloWorld.php
@@ -0,0 +1,10 @@
+<?php
+namespace Ves\HelloWorld\Block;
+
+class HelloWorld extends \Magento\Framework\View\Element\Template
+{
+	public function _prepareLayout()
+	{
+		return parent::_prepareLayout();
+	}
+}
