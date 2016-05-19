f70e5f88a0458292347824a1e6a1ea23f9ff41a9
Loke Carlsson <lc222ak@student.lnu.se>
2016-05-19 12:30:42 +0200

    Added logic to fetch category and product information

---
+++ app/code/Loke/FBC/Block/FBC.php
@@ -0,0 +1,33 @@
+<?php
+namespace Loke\FBC\Block;
+class FBC extends \Magento\Framework\View\Element\Template
+{
+    protected $_registry;
+
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        array $data = []
+    )
+    {
+        $this->_registry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    public function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+
+    public function getCurrentCategory()
+    {
+        return $this->_registry->registry('current_category');
+    }
+
+    public function getCurrentProduct()
+    {
+        return $this->_registry->registry('current_product');
+    }
+
+}
+?>
